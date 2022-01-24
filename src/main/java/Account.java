c488cc7d7d99709a9eb00d130eebd33149c929b2
Karina Palanchuk <lbmmrt@yandex.ru>
2022-01-24 20:27:10 +0300

    account class test

---
+++ src/main/java/Account.java
@@ -0,0 +1,15 @@
+public class Account {
+    private final String name;
+
+    public Account(String name) {
+        this.name = name;
+    }
+
+    public boolean checkNameToEmboss() {
+        if (name.length() < 3 || name.length() > 19 || name.startsWith(" ") || name.endsWith(" ") || !name.contains(" ")) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
