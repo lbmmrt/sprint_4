05c54a80d686fc9a619a74d7144e3eb52afc18fd
Karina Palanchuk <lbmmrt@yandex.ru>
2022-01-26 17:12:17 +0300

    Revert "added regular expression and new tests"
    This reverts commit e739b08dbbff5ec7acffc43f45b8c8d5a2b38639.

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,20 +1,15 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Account {
-
     private final String name;
 
     public Account(String name) {
         this.name = name;
     }
 
-    String regexp = "^(?=.{3,19}$)[а-яёА-ЯЁa-zA-Z]+\\s[а-яёА-ЯЁa-zA-Z]+$";
-    Pattern pattern = Pattern.compile(regexp);
-
-
     public boolean checkNameToEmboss() {
-        Matcher matcher = pattern.matcher(name);
-        return matcher.matches();
+        if (name.length() < 3 || name.length() > 19 || name.startsWith(" ") || name.endsWith(" ") || !name.contains(" ")) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
